b66b4eac983896c917c9c2acbd5463b22d7de50a
deanvry <deanrayhan178@gmail.com>
2020-09-06 18:27:00 +0700

    patching font2

--- config.def.h
+++ config.def.h
@@ -6,6 +6,12 @@
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
 static char *font = "mono:pixelsize=12:antialias=true:autohint=true";
+/* Spare fonts */
+static char *font2[] = {
+    /* examples */
+    "Feather:style=Regular:antialias=true:autohint=true",
+    "Twitter Color Emoji:style=Regular:antialias=true:autohint=true",
+};
 static int borderpx = 7;
 
 /*
